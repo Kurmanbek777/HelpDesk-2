14f778af5b6ace14e634f8283fe69636fe540b70
Kurmanbek777 <knurkamil@gmail.com>
2017-01-27 23:21:33 +0600

    скрипт очистки

---
+++ Backup/HelpDesk/clear.js
@@ -0,0 +1,10 @@
+﻿function clear_Fields() {
+    document.getElementById("fnameTB").value = '';
+    document.getElementById("lnameTB").value = '';
+    document.getElementById("emailTB").value = '';
+    document.getElementById("ddlSeverity").options.length = 0;
+    document.getElementById("ddlStatus").options.length = 0;
+    document.getElementById("ddlDept").options.length = 0;
+    document.getElementById("commentsTB").value = '';
+    document.getElementById("ddlEmp").options.length = 0;
+}
